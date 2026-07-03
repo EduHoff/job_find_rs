fecbfc25f17a6788412884d3071d28b8db80b098
EduHoff <ehcs.business@gmail.com>
2026-07-03 12:46:44 -0300

    feat: integração inicial entre o Rust e Python

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use pyo3::ffi::c_str;
+use pyo3::prelude::*;
+
+fn main() -> PyResult<()> {
+    Python::with_gil(|py| {
+        py.run(c_str!(include_str!("hello.py")), None, None)?;        
+        Ok(())
+    })
 }
